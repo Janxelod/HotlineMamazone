af68187f0d717175298612ee47af060a880e7c0e
Joan Odicio <yusuke.joan@gmail.com>
2016-12-12 11:50:59 +0100

    Added Good Ending GameObjects

--- Assets/Scripts/GamerOverManager.cs
+++ Assets/Scripts/GamerOverManager.cs
@@ -5,8 +5,9 @@ public class GamerOverManager : MonoBehaviour {
 
 	public GameObject boss;
 	public GameObject windows;
-	public GameObject popupMediumWin;
-	public GameObject pupUpBadEnding;
+	public GameObject popUpMediumEnding;
+	public GameObject popUpBadEnding;
+	public GameObject popUpGoodEnding;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,16 +15,21 @@ public class GamerOverManager : MonoBehaviour {
 	public void StartGameOver(){
 		gameObject.SetActive(true);
 		boss.SetActive(true);
-		pupUpBadEnding.SetActive (true);
+		popUpBadEnding.SetActive (true);
 		windows.SetActive(false);
 		GetComponents<AudioSource>()[0].Play();
 	}
 	public void StartMediumEnding() {
 		gameObject.SetActive(true);
-		popupMediumWin.SetActive(true);
-
+		popUpMediumEnding.SetActive(true);
 		GameObject.Find("Messenger").SetActive(false);
 		GetComponents<AudioSource>()[1].Play();
+	}
+	public void StartGoodEnding() {
+		gameObject.SetActive(true);
+		popUpGoodEnding.SetActive(true);
+		//GameObject.Find("Messenger").SetActive(false);
+		GetComponents<AudioSource>()[2].Play();
 	}
 	// Update is called once per frame
 	void Update () {
